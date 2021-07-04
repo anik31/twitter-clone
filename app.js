bbd5aacb16e920286ba430abcadbd02b4b92793d
Aniket Prakash <b218004@iiit-bh.ac.in>
2021-07-04 08:41:14 +0530

    Added authentication

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const session = require('express-session')
+const passport = require('passport')
+const localStrategy = require('passport-local')
+const User = require('./models/users')
+const {isLoggedIn} = require('./middleware')
+const flash = require('connect-flash')
+
 const app = express()
 
 mongoose.connect('mongodb://localhost:27017/twitter-clone', {
@@ -13,9 +20,28 @@ mongoose.connect('mongodb://localhost:27017/twitter-clone', {
 .catch(err => console.log(err))
 
 app.set('view engine','ejs')
-// app.set('views',path.join(__dirname+'/views'))
 app.use(express.static(path.join(__dirname+'/public')))
-app.get('/',(req,res) => res.render('home'))
+app.use(express.urlencoded({extended:true}))
+
+//Routes
+const auth = require("./routes/auth")
+
+app.use(session({
+    secret: 'lenovoideapad',
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new localStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+app.use(auth)
+  
+app.get('/',isLoggedIn,(req,res) => res.render('home'))
 
 
 app.listen(3000, () => console.log('Server running on port 3000'))
---
+++ routes/auth.js
@@ -0,0 +1,38 @@
+const router = require('express').Router()
+const User = require('../models/users');
+const passport = require('passport')
+const {isLoggedIn} = require('../middleware')
+
+//GET register form
+router.get('/register',(req,res) => {
+                res.render('auth/signUp',{message:req.flash('error')})
+            }) 
+//POST register form
+router.post('/register',async(req,res)=>{
+    try{
+    const user = {
+        firstName : req.body.firstname,
+        lastName : req.body.lastname,
+        email : req.body.email,
+        username : req.body.username
+        }
+        const newUser = await User.register(user,req.body.password)
+        res.send(newUser)
+    }catch(err){
+        req.flash('error',err.message)
+        res.redirect('/register')
+    }
+})
+//Login
+router.get('/login',(req,res)=> res.render('auth/login'))
+router.post('/login', passport.authenticate('local',
+{
+    failureRedirect : '/login'
+}),(req,res)=> res.redirect('/'))
+
+//logout
+router.get('/logout',(req,res)=>{
+    req.logout()
+    res.redirect('/login')
+})
+module.exports = router
